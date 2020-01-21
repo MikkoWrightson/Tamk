9818bc9e569a7d542c2d606e575e1735786ee2ac
Mikko Wrightson <Mikko.Wrightson@tuni.fi>
2020-01-21 10:19:03 +0200

    Add assignment 2

--- ohjelmointiEdistyneetPiirteet/vko3/ratkaisu_vko3_teht2.cpp
+++ ohjelmointiEdistyneetPiirteet/vko3/ratkaisu_vko3_teht2.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <math.h>
+
+void roundUpWeedKiller(float input);
 
 int main(){
-
-
+    float input;
+    std::cout << "Input a value: ";
+    std::cin >> input;
+    roundUpWeedKiller(input);
     return 0;
+}
+
+void roundUpWeedKiller(float input){
+    std::cout << "The given value " << input << " lies between " << floor(input) << " and " << ceil(input) << std::endl;
 }
