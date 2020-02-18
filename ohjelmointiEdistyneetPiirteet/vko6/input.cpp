d45c4a71c5e2f4fabd6238f70792a58b40b64b1e
Mikko Wrightson <Mikko.Wrightson@tuni.fi>
2020-02-18 13:14:04 +0200

    cleaning stuff up

--- ohjelmointiEdistyneetPiirteet/vko6/input.cpp
+++ ohjelmointiEdistyneetPiirteet/vko6/input.cpp
@@ -1,9 +1,6 @@
 #include "func.h"
 #include <iostream>
-#include <curses.h>
-#include <fstream>
-#include <memory>
-#include <limits>
+//#include <limits>
 
 using namespace std;
 
@@ -11,6 +8,7 @@ using namespace std;
 void lueRivi(int rivi[]){
     std::cout << "Anna lottonumerot: ";
     std::cin >> rivi[0] >> rivi[1] >> rivi[2] >> rivi[3] >> rivi[4] >> rivi[5] >> rivi[6];
+    std::cin.ignore();
 }
 
 
@@ -26,6 +24,7 @@ int valikko(){
     int input;
     std::cout << "1) Lue ja tulosta lottorivi\n2) Lopeta ohjelman ajo\nAnna valintasi(1/2): ";
     std::cin >> input;
+    std::cin.ignore();
     return input;
 }
 
@@ -38,43 +37,7 @@ void riviTulostus(int rivi[], int plusNro){
     std::cout << " ja plusnumero: " << plusNro;
     std::cout << "\n\tPaina ENTER jatkaaksesi ...";
     std::cin.get();
-
-  //std::cout << "Press Enter to Continue" << std::endl;
-  //std::cin.sync();
-  //std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-
-    
-    /*
-    std::cout << "Press Enter to Continue" << std::endl;
-    std::cin.sync();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-    */
-    //std::getline(std::cin, temp);
-    //cin.clear(iostream::failbit);
-    //waitForEnter();
-    //std::cin.clear;
-    /*
-    do 
-    {
-        std::cout << '\n' << "Press a key to continue...";
-    } while (cin.get() != '\n');
-    */
-    
-    //getchar();
-    //system("pause");
-    //temp = std::cin.get();// miten saan sut toimii hhä hä jatka tästä
-    //std::cin.ignore();
     
     std::cout << std::endl;
-
-}
-
-
-void waitForEnter(){
-  initscr();
-  cbreak();
-  printw("press any key to exit...");
-  getch();
-  endwin();
 
 }
